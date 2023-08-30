fa1798930e611c4ba1f9236886c8d727f808a0f8
kian kheirani <kiankheirani@gmail.com>
2023-08-30 14:50:48 +0430

    'add-router'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./layout/layout";
+import Home from "./pages/Home";
+import About from "./pages/about";
 function App() {
-  return <div>salamde</div>;
+  const rout = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<RootLayout></RootLayout>}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Route>
+    )
+  );
+  return <RouterProvider router={rout}></RouterProvider>;
 }
 
 export default App;
